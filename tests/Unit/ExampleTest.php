ce111484a495eea779c5099df5a5c18e4ce80d2f
Hung Tran <hung.tran@seldatinc.com>
2017-06-26 16:02:19 +0700

    Update Unitest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -26,20 +26,35 @@ class ExampleTest extends TestCase
     {
         //GIVEN
         // 2posts 1 on this month and 1 last month
-        factory(Post::class)->create([
-            'created_at' => Carbon::now()->subMonth()
-        ]);
         
-        factory(Post::class)->create([
+        
+        $first= factory(Post::class)->create([
             'created_at' => Carbon::now()
         ]);
         
+        $second  = factory(Post::class)->create([
+            'created_at' => Carbon::now()->subMonth()
+        ]);
         
         //WHEN
         // I fetch post
-        $posts = Post::archives();
+        $posts = Post::archives();        
         
         //THEN
-        $this->assertCount(2, $posts);
+        $this->assertEquals([
+            0 => [
+                "year" => $first->created_at->format('Y'),
+                "month" => $first->created_at->format('F'),
+                "published" => 1,
+            ],
+            1 => [
+                "year" => $second->created_at->format('Y'),
+                "month" => $second->created_at->format('F'),
+                "published" => 1,
+            ]
+        ]
+        , $posts);
+        
+        //$this->assertCount(2, $posts);
     }
 }
