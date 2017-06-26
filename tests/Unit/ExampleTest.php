b25ee693feff218d3e6c6c346a30803908abfaf9
Hung Tran <hung.tran@seldatinc.com>
2017-06-26 15:52:31 +0700

    Add UnitTest Post::archives

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
+use App\Post;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
@@ -13,8 +15,29 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
+//    public function testBasicTest()
+//    {
+//        $this->assertTrue(true);
+//    }
+    
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        //GIVEN
+        // 2posts 1 on this month and 1 last month
+        factory(Post::class)->create([
+            'created_at' => Carbon::now()->subMonth()
+        ]);
+        
+        factory(Post::class)->create([
+            'created_at' => Carbon::now()
+        ]);
+        
+        
+        //WHEN
+        // I fetch post
+        $posts = Post::archives();
+        
+        //THEN
+        $this->assertCount(2, $posts);
     }
 }
